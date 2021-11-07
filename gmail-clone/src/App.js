579770bf31a73955cb8ccf5b8a8b1fa2348964cc
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-11-07 18:03:17 +0530

    sidebarOptions

--- gmail-clone/src/App.js
+++ gmail-clone/src/App.js
@@ -2,14 +2,34 @@ import React from 'react';
 import './App.css';
 import Header from './Containers/Header';
 import Sidebar from './Containers/Sidebar';
+import Mail from './Containers/Mail';
+import EmailList from './Components/EmailList';
+import {
+  BrowserRouter as Router,
+  Routes ,
+  Route,
+  // Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      {/* <h2>Building Gmail</h2> */}
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Routes >
+            <Route path = "/mail">
+              <Mail />
+            </Route>
+            <Route path = "/">
+              <EmailList />
+            </Route>
+          </Routes >
+        </div>
+        {/* <h2>Building Gmail</h2> */}
+      </div>
+    </Router>
   );
 }
 
